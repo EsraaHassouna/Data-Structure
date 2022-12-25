775e0ce7e8918dbcff3b286e757e3e17fc43f13a
EsraaHassouna <esraa.hassouna9.11@gmail.com>
2022-12-26 01:16:06 +0200

    first commit

---
+++ linked/lab1_DS/linkedList.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+
+int main()
+{
+
+    Add(13);
+    Add(7);
+    Add(10);
+    Add(50;
+
+    Display();
+
+    printf("\n%d",GetCount());
+
+    GetDataByIndex(2);
+
+    printf("\n");
+
+    InsertAfter(10, 7);
+
+    Display();
+
+    return 0;
+}
