0960726e4783e846816bb26a8e21a6d04cfae8b7
EsraaHassouna <esraa.hassouna9.11@gmail.com>
2022-12-27 08:37:39 +0200

    queue

---
+++ Queue/main.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int main()
+{
+    Queue *q = (Queue*)malloc(sizeof(Queue));
+
+    q->front = NULL;
+    q->rear = NULL;
+    q->length = 0;
+
+    EnQueue(q, 1);
+    EnQueue(q, 2);
+    EnQueue(q, 3);
+
+    DeQueue(q);
+    DeQueue(q);
+
+    display(q);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
---
+++ Queue/queue.h
@@ -0,0 +1,68 @@
+
+
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#define MaxQSize 6
+typedef struct Node
+{
+    int data;
+    struct Node *prevNode;
+} Node;
+typedef struct Queue
+{
+    int length;
+    Node *front;
+    Node *rear;
+} Queue;
+
+void EnQueue(Queue *q, int data)
+{
+
+    if(q->length == MaxQSize)
+        return;
+
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+
+    q->length += 1;
+
+    if(q->length != 1)
+    {
+        newNode->prevNode = q->front;
+        q->front = newNode;
+
+    }
+    else
+    {
+        newNode->prevNode = NULL;
+        q->rear = newNode;
+        q->front = newNode;
+    }
+}
+
+void DeQueue(Queue *q)
+{
+    if(q->length == 0)
+    {
+        return;
+    }
+    else
+    {
+        q->rear->data = q->front[1].data;
+        q->length -= 1;
+    }
+
+}
+
+void display(Queue * q)
+{
+    int i;
+    for(i = 0; i < q->length; i++)
+    {
+        printf(" -> %d", q->front->data);
+        q->front = q->front->prevNode;
+    }
+}
+#endif // QUEUE_H
