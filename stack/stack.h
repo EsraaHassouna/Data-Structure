315f8bf78c735de32cfe4d1f8218071526b11f40
EsraaHassouna <esraa.hassouna9.11@gmail.com>
2022-12-27 08:23:44 +0200

    stack

---
+++ stack/stack.h
@@ -0,0 +1,84 @@
+#ifndef STACK_H
+#define STACK_H
+
+#define MaxSize 5
+
+typedef struct Node Node;
+
+struct Node
+{
+    int Data;
+    Node *Next;
+};
+
+Node* top = NULL;
+
+typedef struct Stack
+{
+    int length;
+    Node *NewNode;
+    Node *top;
+} Stack;
+
+int Push(Stack *stk, int data)
+{
+    if(stk->length == MaxSize)
+        return 0;
+
+    Node *newNode = malloc(sizeof(Node));
+    newNode->Data = data;
+    newNode->Next = NULL;
+
+    if(top == NULL)
+    {
+        top = newNode;
+    }
+    else
+    {
+        newNode->Next=top;
+    }
+    top = newNode;
+
+
+    stk->top = top;
+    stk->length += 1;
+    stk->NewNode= newNode;
+
+
+
+    printf("Node %d Done\n\n", newNode->Data);
+    return 1;
+}
+
+int Pop(Stack *s)
+{
+    if(top == NULL)
+    {
+        return 0;
+    }
+
+    printf("Node %d is poped\n\n", s->NewNode->Data);
+
+    s->NewNode = s->NewNode->Next;
+    s->length -= 1;
+    s->top = s->NewNode;
+
+    return 1;
+}
+
+void display(Stack *s)
+{
+    do
+    {
+
+        printf(" -> %d", s->NewNode->Data);
+        s->NewNode = s->NewNode->Next;
+
+    }
+    while(s->NewNode->Next != NULL);
+
+    printf(" -> %d", s->NewNode->Data);
+}
+
+#endif // STACK_H
+
